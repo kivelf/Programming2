3809a1b1843b989ba4b504c47996ae738c153f44
Remy <rumena.mp@gmail.com>
2023-08-21 11:11:09 +0200

    test of branches

--- L00 Git 1 Klassehierarkier 1/src/branchesTest/BranchesTest.java
+++ L00 Git 1 Klassehierarkier 1/src/branchesTest/BranchesTest.java
@@ -4,5 +4,6 @@ public class BranchesTest {
     public static void main(String[] args) {
         System.out.println("Testing branches here...");
         System.out.println("This is the first test branch");
+        System.out.println("Branch B is here");
     }
 }
