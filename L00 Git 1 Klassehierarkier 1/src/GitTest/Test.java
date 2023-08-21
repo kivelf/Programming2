521554ba6bbd18d8820043c3b4a495208cbe29bc
Remy <rumena.mp@gmail.com>
2023-08-21 09:17:49 +0200

    adding a line

--- L00 Git 1 Klassehierarkier 1/src/GitTest/Test.java
+++ L00 Git 1 Klassehierarkier 1/src/GitTest/Test.java
@@ -3,5 +3,6 @@ package GitTest;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Calling the Mothership...");
+        System.out.println("Give me one ping, Vassily");
     }
 }
