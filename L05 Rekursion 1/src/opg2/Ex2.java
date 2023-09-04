e9b795244a9a0f93fabecd5f5a70b60aa6b42493
Remy <rumena.mp@gmail.com>
2023-09-04 10:25:57 +0200

    exercise 2 from 04/09

--- L05 Rekursion 1/src/opg2/Ex2.java
+++ L05 Rekursion 1/src/opg2/Ex2.java
@@ -1,12 +1,15 @@
 package opg2;
 
+import java.math.BigInteger;
+
 public class Ex2 {
     public static void main(String[] args) {
         int n = 2;
         for (int p = 0; p <= 10; p++){
-            System.out.printf("n: %1d    to the power of %2d = %,d\n", n, p, power(n, p));
+            System.out.printf("n: %1d    to the power of %2d = %,d\n", n, p, power(BigInteger.valueOf(n), BigInteger.valueOf(p)));
         }
 
+        System.out.println(power(BigInteger.valueOf(2), BigInteger.valueOf(128)));
         System.out.println();
         System.out.println("Testing the second method:");
         for (int p = 0; p <= 10; p++){
@@ -15,11 +18,11 @@ public class Ex2 {
     }
 
     /** Calculates n^p for p >= 0. */
-    public static int power(int n, int p){
-        if (p == 0){
-            return 1;
+    public static BigInteger power(BigInteger n, BigInteger p){
+        if (p.intValue() == 0){
+            return BigInteger.valueOf(1);
         } else {
-            return n * (power(n, p - 1));
+            return n.multiply(power(n, p.subtract(BigInteger.valueOf(1))));
         }
     }
 
--- L05 Rekursion 1/src/opg3/Ex3.java
+++ L05 Rekursion 1/src/opg3/Ex3.java
@@ -2,7 +2,10 @@ package opg3;
 
 public class Ex3 {
     public static void main(String[] args) {
-
+        System.out.println("3 * 5 is " + prod(3, 5));
+        System.out.println("12 * 8 is " + prod(12, 8));
+        System.out.println("7 * 6 is " + prod(7, 6));
+        System.out.println("18 * 5 is " + prod(18, 5));
     }
 
     /** Calculates a * b for a and b >= 0. */
@@ -12,7 +15,7 @@ public class Ex3 {
         } else if (a == 1){
             return b;
         } else {
-            return prod(a - 1, b);
+            return prod(a - 1, b) + b;
         }
     }
 }
