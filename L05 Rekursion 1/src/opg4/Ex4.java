30f95f4ec904fd4500083ed29b020bcf55a22afb
Remy <rumena.mp@gmail.com>
2023-09-04 11:51:33 +0200

    exercise 4 from 04/09

---
+++ L05 Rekursion 1/src/opg4/Ex4.java
@@ -0,0 +1,23 @@
+package opg4;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        System.out.println("The reverse of 'RANSLIRPA' is " + reverse("RANSLIRPA"));    // uneven
+        System.out.println("The reverse of 'SRANSLIRPA' is " + reverse("SRANSLIRPA"));    // even
+    }
+
+    /** Returns the string with the characters in reverse order.
+     * String is longer than 0. */
+    public static String reverse(String s){
+        String reversed = s;
+        if (s.length() == 2){
+            reversed = s.charAt(1) + "" + s.charAt(0);
+        } else if (s.length() <= 1){
+            // the char in the middle is already correct, so no need to do anything
+        }
+        else {
+            reversed = s.charAt(s.length() - 1) + reverse(s.substring(1, s.length() - 1)) + s.charAt(0);
+        }
+        return reversed;
+    }
+}
