3671b1cb0a1a183216ae7c697aba57e2d077e122
Remy <rumena.mp@gmail.com>
2023-09-07 08:46:24 +0200

    exercise 4 from 04/09

--- L05 Rekursion 1/src/opg4/Ex4.java
+++ L05 Rekursion 1/src/opg4/Ex4.java
@@ -10,9 +10,7 @@ public class Ex4 {
      * String is longer than 0. */
     public static String reverse(String s){
         String reversed = s;
-        if (s.length() == 2){
-            reversed = s.charAt(1) + "" + s.charAt(0);      // swapping the final 2 chars
-        } else if (s.length() == 1){
+        if (s.length() <= 1){
             // the char in the middle is already correct, so no need to do anything
         }
         else {
