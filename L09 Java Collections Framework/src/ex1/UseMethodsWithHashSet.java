760d1098d93bd10f2ed012567af9d42c9f07bf9e
Remy <rumena.mp@gmail.com>
2023-09-19 10:16:50 +0200

    exercise 1 from 19/09

---
+++ L09 Java Collections Framework/src/ex1/UseMethodsWithHashSet.java
@@ -0,0 +1,33 @@
+package ex1;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class UseMethodsWithHashSet {
+    public static void main(String[] args) {
+        // a)
+        HashSet<Integer> set = new HashSet<>();
+        // b)
+        set.add(34);
+        set.add(12);
+        set.add(23);
+        set.add(45);
+        set.add(67);
+        set.add(34);
+        set.add(98);
+        // c)
+        System.out.println("Set: " + set);
+        // d)
+        set.add(23);
+        // e)
+        System.out.println("Set: " + set);
+        // f)
+        set.remove(67);
+        // g)
+        System.out.println("Set: " + set);
+        // h)
+        System.out.println("Set contains 23? " + set.contains(23));
+        // i)
+        System.out.println("Amount of numbers in the set: " + set.size());
+    }
+}
