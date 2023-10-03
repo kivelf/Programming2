fd6c7be05930c5f0f3314232c76ee4da180c7a56
Remy <rumena.mp@gmail.com>
2023-10-03 12:01:59 +0200

    exercise 5 from 25/09

--- L11 Strategy pattern/src/ex5/Test.java
+++ L11 Strategy pattern/src/ex5/Test.java
@@ -3,11 +3,11 @@ package ex5;
 public class Test {
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
-        cart.addItem(new Item("Sucker", 10));
+        cart.addItem(new Item("Sugar", 10));
         cart.addItem(new Item("Beers", 40));
         // pay by credit card
         cart.pay(new CreditCardPayment("John Doe", "1234567890123456"));
-        // pay by PPaypal
+        // pay by Paypal
         cart.pay(new PaypalPayment("johndoe@example.com", "MyPassword"));
     }
 }
