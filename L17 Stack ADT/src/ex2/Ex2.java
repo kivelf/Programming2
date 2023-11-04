3510f650a6ca13be8b2c9c5c5302e43802b0ab6c
Remy <rumena.mp@gmail.com>
2023-11-04 13:31:03 +0100

    exercise 2 from 02/11

---
+++ L17 Stack ADT/src/ex2/Ex2.java
@@ -0,0 +1,49 @@
+package ex2;
+
+import stack23ystudent.ArrayStack23Y;
+import stack23ystudent.Stack23Y;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        String stringToTest1 = "(3+{5[99 {*}]}[23[{67}67]])";
+        String stringToTest2 = "(}){";
+
+        System.out.println("Does string '(3+{5[99 {*}]}[23[{67}67]])' contain matching brackets? "
+                + checkMatchingBrackets(stringToTest1));     // true
+        System.out.println("Does string '(}){' contain matching brackets? "
+                + checkMatchingBrackets(stringToTest2));     // false
+    }
+
+    public static boolean checkMatchingBrackets(String stringToTest){
+        Stack23Y stack = new ArrayStack23Y();
+        boolean bracketsMatch = true;
+        boolean keepGoing = true;
+
+        while (bracketsMatch && keepGoing){
+            for (int i = 0; i < stringToTest.length(); i++){
+                if (stringToTest.charAt(i) == 40 || stringToTest.charAt(i) == 91 || stringToTest.charAt(i) == 123){
+                    stack.push(stringToTest.charAt(i));
+                } else if (stringToTest.charAt(i) == 41){
+                    if ((char) stack.peek() == 40){
+                        stack.pop();
+                    } else {
+                        bracketsMatch = false;
+                        keepGoing = false;
+                    }
+                } else if (stringToTest.charAt(i) == 93 || stringToTest.charAt(i) == 125){
+                    if ((char) stack.peek() == (stringToTest.charAt(i) - 2)){
+                        stack.pop();
+                    } else {
+                        bracketsMatch = false;
+                        keepGoing = false;
+                    }
+                }
+                if (i == stringToTest.length() - 1){
+                    keepGoing = false;
+                }
+            }
+        }
+
+        return bracketsMatch;
+    }
+}
